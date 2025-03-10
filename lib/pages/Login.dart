a7e226899aa835ed6d81110a7982cb2a3475c6e5
unknown <reemseif139@gmai.com>
2025-03-10 10:17:32 +0200

    last

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_material_design_icons/flutter_material_design_icons.dart';
 
@@ -11,6 +13,9 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   bool isPasswordVisible = true;
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  var formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,72 +24,104 @@ class _LoginState extends State<Login> {
         centerTitle: true,
         backgroundColor:  Colors.indigo,
       ),
-      body: Column(
-        children: [
-          Text('User Login' , style: 
-          TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),),
-          SizedBox(height: 20,),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                hintText: 'Enter your email',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(MdiIcons.email),
-                prefixIconColor: Colors.black
+      body: Form(
+        key: formkey,
+        child: Column(
+          children: [
+            Text('User Login' , style: 
+            TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),),
+            SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                controller: emailcontroller,
+                validator: (value){
+                  if(value!.isEmpty){
+                    return 'Please enter your email';
+                  }
+                  final bool isValid = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value);
+                    if(!isValid){
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Enter your email',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(MdiIcons.email),
+                  prefixIconColor: Colors.black
+                ),
               ),
             ),
-          ),
-           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              obscureText: isPasswordVisible,  
-              decoration: InputDecoration(
-                labelText: 'Password',
-                hintText: 'Enter your password',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(MdiIcons.formTextboxPassword),
-                prefixIconColor: Colors.black,
-                suffixIcon: IconButton(onPressed: (){
-                  setState(() {
-                    isPasswordVisible = !isPasswordVisible;
-                  });
-                }, icon: Icon(isPasswordVisible ? MdiIcons.eye : MdiIcons.eyeOff)),
+             Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                obscureText: isPasswordVisible,  
+                keyboardType: TextInputType.visiblePassword,
+                controller: passwordcontroller,
+                validator: (value){
+                  if(value!.isEmpty){
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(MdiIcons.formTextboxPassword),
+                  prefixIconColor: Colors.black,
+                  suffixIcon: IconButton(onPressed: (){
+                    setState(() {
+                      isPasswordVisible = !isPasswordVisible;
+                    });
+                  }, icon: Icon(isPasswordVisible ? MdiIcons.eye : MdiIcons.eyeOff)),
+                  
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              width: 350,
+              child: ElevatedButton(
+                onPressed: () {
+                  if(formkey.currentState!.validate())
+                  {
+                      print(emailcontroller.text);
+                  print(passwordcontroller.text);
+                  emailcontroller.clear();
+                  passwordcontroller.clear();
+                  }
                 
-              ),
+                  
+                },
+                child: Text('Login', style: TextStyle(
+                    color: Colors.blue,
+                  ),),
+               ),
             ),
-          ),
-          SizedBox(height: 20,),
-          Container(
-            width: 350,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text('Login', style: TextStyle(
-                  color: Colors.blue,
-                ),),
-             ),
-          ),
-          SizedBox(height: 20,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Don\'t have an account?'),
-              TextButton(
-                onPressed: (){},
-                child: Text('Register Now' ,
-                style: TextStyle(
-                  color: Colors.blue,
-                      decoration: TextDecoration.underline,
-                ),),
-              ),
-            ],
-          ),
-        ],
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Don\'t have an account?'),
+                TextButton(
+                  onPressed: (){},
+                  child: Text('Register Now' ,
+                  style: TextStyle(
+                    color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                  ),),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
